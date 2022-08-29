b9d769a2f88caa71e0a545b074b081826842605e
neglux <akcaaytuna@gmail.com>
2022-08-29 11:21:39 +0300

    push method implemented

--- sll.js
+++ sll.js
@@ -11,4 +11,14 @@ class SinglyLinkedList {
     this.tail = null;
     this.length = 0;
   }
+
+  push(val) {
+    const newNode = new Node(val);
+    if (!this.head) this.head = newNode;
+    else this.tail.next = newNode;
+
+    this.tail = newNode;
+    this.length++;
+    return this;
+  }
 }
