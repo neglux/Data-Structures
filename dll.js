5f9831e3aafd126f0389467813170b1ecb394e9b
neglux <akcaaytuna@gmail.com>
2022-08-30 09:26:33 +0300

    implementing push method

--- dll.js
+++ dll.js
@@ -12,4 +12,15 @@ class DoublyLinkedList {
     this.tail = null;
     this.length = 0;
   }
+  push(val) {
+    const newNode = new Node(val);
+    if (!this.head) this.head = newNode;
+    else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+    }
+    this.tail = newNode;
+    this.length++;
+    return this;
+  }
 }
