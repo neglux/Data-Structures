739a406386ffe109ceed3d93f09dc02141bfeb0b
neglux <akcaaytuna@gmail.com>
2022-08-30 12:42:19 +0300

    implement insert method to bst

--- bst.js
+++ bst.js
@@ -10,4 +10,30 @@ class BinarySearchTree {
   constructor() {
     this.root = null;
   }
+  insert(val) {
+    const newNode = new Node(val);
+    if (!this.root) {
+      this.root = newNode;
+      return this;
+    }
+
+    let node = this.root;
+    while (true) {
+      if (newNode.val === node.val) return undefined;
+
+      if (newNode.val > node.val) {
+        if (node.right) node = node.right;
+        else {
+          node.right = newNode;
+          return this;
+        }
+      } else {
+        if (node.left) node = node.left;
+        else {
+          node.left = newNode;
+          return this;
+        }
+      }
+    }
+  }
 }
