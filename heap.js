6281742bd845bb03f9acc452be52a1fb41b8c2b4
neglux <akcaaytuna@gmail.com>
2022-09-01 16:33:46 +0300

    implementing extractMax method

--- heap.js
+++ heap.js
@@ -21,4 +21,52 @@ class MaxBinaryHeap {
     this.values.push(val);
     bubbleUp();
   }
+  extractMax() {
+    const swap = (arr, index1, index2) => {
+      let temp = arr[index1];
+      arr[index1] = arr[index2];
+      arr[index2] = temp;
+      return true;
+    };
+    const getMax = (obj1, obj2) => {
+      if (Math.max(obj1.val, obj2.val) === obj1.val) return obj1;
+      return obj2;
+    };
+    const sinkDown = () => {
+      let parentIndex = 0;
+      while (true) {
+        let swapIx = null;
+        const cur = {
+          val: this.values[parentIndex],
+          leftChild:
+            {
+              index: 2 * parentIndex + 1,
+              val: this.values[2 * parentIndex + 1],
+            } || null,
+          rightChild:
+            {
+              index: 2 * parentIndex + 2,
+              val: this.values[2 * parentIndex + 2],
+            } || null,
+        };
+
+        if (cur.rightChild.val)
+          swapIx = getMax(cur.leftChild, cur.rightChild).index;
+        else {
+          if (cur.leftChild.val && cur.leftChild.val > cur.val)
+            swapIx = cur.leftChild.index;
+        }
+
+        if (swapIx === null) return;
+        swap(this.values, swapIx, parentIndex);
+        parentIndex = swapIx;
+      }
+    };
+    if (!this.values.length) return undefined;
+
+    swap(this.values, 0, this.values.length - 1);
+    const removedElement = this.values.pop();
+    sinkDown();
+    return removedElement;
+  }
 }
