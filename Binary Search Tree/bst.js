27ece37322e62809aca1a82e420cdc6ee494ea0e
neglux <akcaaytuna@gmail.com>
2022-09-01 10:57:31 +0300

    implementing delete method

--- Binary Search Tree/bst.js
+++ Binary Search Tree/bst.js
@@ -52,4 +52,54 @@ class BinarySearchTree {
       }
     }
   }
+  delete(val) {
+    const deleteLeaf = (parent, removedNode) => {
+      if (removedNode === parent.left) parent.left = null;
+      else parent.right = null;
+      return removedNode;
+    };
+
+    const deleteNodeWithOneChild = (parent, removedNode) => {
+      if (removedNode === parent.left)
+        parent.left = removedNode.left || removedNode.right;
+      else parent.right = removedNode.left || removedNode.right;
+      return removedNode;
+    };
+
+    const deleteNodeWithTwoChild = (parent, removedNode) => {
+      const findMin = (node) => {
+        node = node.right;
+        while (node) {
+          if (node.left) node = node.left;
+          else return node;
+        }
+      };
+
+      const newNode = findMin(removedNode);
+      this.delete(newNode.val);
+      newNode.left = removedNode.left;
+      if (removedNode.right === newNode) newNode.right = null;
+      else newNode.right = removedNode.right;
+
+      if (!parent) this.root = newNode;
+      else {
+        if (parent.left === removedNode) parent.left = newNode;
+        else parent.right = newNode;
+      }
+      return removedNode;
+    };
+    let cur = this.root;
+    let parent = null;
+    while (true) {
+      if (val === cur.val) {
+        if (!cur.left && !cur.right) return deleteLeaf(parent, cur);
+        if (!cur.left || !cur.right) return deleteNodeWithOneChild(parent, cur);
+        return deleteNodeWithTwoChild(parent, cur);
+      }
+
+      parent = cur;
+      if (val > cur.val) cur = cur.right;
+      else cur = cur.left;
+    }
+  }
 }
